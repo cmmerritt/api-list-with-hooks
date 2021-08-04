54b2900f94cfd69ebb159802c278922a21ceced1
cmmerritt <christiane.merritt@gmail.com>
2021-08-04 10:52:27 -0700

    add links to etail pages

--- src/components/queens/Queen.jsx
+++ src/components/queens/Queen.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-const Queen = ({ name, image, quote }) => (
+const Queen = ({ name, image, quote, id }) => (
   <figure>
-    <img src={image} alt={name} height="200px"/>
-    <figcaption>
-      <p>{name}</p>
-      <p>&quot;{quote}&quot;</p>
-    </figcaption>
+    <Link to={`/queen/${id}`}>
+      <img src={image} alt={name} height="200px"/>
+      <figcaption>
+        <p>{name}</p>
+        <p>&quot;{quote}&quot;</p>
+      </figcaption>
+    </Link>
   </figure>
 );
 
@@ -15,6 +18,7 @@ Queen.propTypes = {
   name: PropTypes.string.isRequired,
   image: PropTypes.string.isRequired,
   quote: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired
 };
 
 export default Queen;
