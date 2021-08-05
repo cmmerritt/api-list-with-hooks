bc1c85cfd6c248fcd83370acc11e9cf7c01aeb0a
cmmerritt <christiane.merritt@gmail.com>
2021-08-05 00:03:28 -0700

    fix path issue

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import React from 'react';
 import DragQueens from '../../containers/DragQueens';
-import QueenDetail from '../queens/QueenDetail';
+import QueenDetail from '../../containers/QueenDetail';
 
 function App() {
   return (
